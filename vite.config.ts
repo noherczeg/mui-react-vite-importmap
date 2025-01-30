20ba67138b9a9aca6c2bea87d12e7767e500aedb
Norbert Csaba Herczeg <noherczeg@gmail.com>
2025-01-30 04:34:54 +0100

    refactor code and config

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         "react-dom": "https://esm.sh/react-dom@19.0.0",
         "react-dom/client": "https://esm.sh/react-dom@19.0.0/client",
         "@mui/material": "https://esm.sh/@mui/material@6.4.1?bundle=true&deps=react@19.0.0,react-dom@19.0.0,@emotion/react@11.14.0,@emotion/styled@11.14.0",
-        "@emotion/react": "https://esm.sh/@emotion/react@11.14.0?bundle=true&deps=hoist-non-react-statics@3.3.2",
+        "@emotion/react": "https://esm.sh/@emotion/react@11.14.0?bundle=true",
         "@emotion/styled": "https://esm.sh/@emotion/styled@11.14.0?bundle=true",
       },
     }),
