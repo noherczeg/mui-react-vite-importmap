7c99a1ca59bb799727bcb26188e25b66e724a401
Norbert Csaba Herczeg <noherczeg@gmail.com>
2025-01-30 04:36:23 +0100

    refactor code and config

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,7 @@ export default defineConfig({
   plugins: [
     vitePluginImportMap({
       mapping: {
-        "scheduler": "https://esm.sh/scheduler@0.25.0",
         "react": "https://esm.sh/react@19.0.0",
-        "react-is": "https://esm.sh/react-is@19.0.0",
         "react/jsx-runtime": "https://esm.sh/react@19.0.0/jsx-runtime",
         "react-dom": "https://esm.sh/react-dom@19.0.0",
         "react-dom/client": "https://esm.sh/react-dom@19.0.0/client",
